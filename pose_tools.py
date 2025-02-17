b501efed988ea9b9dabbc34c964deffbaaeed341
anay-schottlabs <anay@schottlabs.com>
2025-02-17 15:26:51 -0800

    Removed default landmark data because it is not needed

--- pose_tools.py
+++ pose_tools.py
@@ -1,11 +1,6 @@
 import mediapipe as mp
 import cv2
 import math
-import json
-
-# Load default pose data
-with open("default_world_landmarks.json", "r") as file:
-  DEFAULT_WORLD_LANDMARKS = json.load(file)
 
 class PoseTracker:
   @staticmethod
@@ -18,17 +13,12 @@ class PoseTracker:
     pose_detection_result = mp_pose.process(image_rgb)
     image_landmarks = pose_detection_result.pose_landmarks
     world_landmarks = pose_detection_result.pose_world_landmarks
-    
-    # If no one is in the frame, use the default world landmarks
-    if world_landmarks:
-      world_landmarks = world_landmarks.landmark
-    else:
-      world_landmarks = DEFAULT_WORLD_LANDMARKS
+
     return image_landmarks, world_landmarks
 
 class PoseVisualizer:
   @staticmethod
-  def show_pose(image, pose_landmarks, world_landmarks, screen_dims):
+  def show_pose(image, image_landmarks, world_landmarks, screen_dims):
     mp_pose = mp.solutions.pose
     mp_drawing = mp.solutions.drawing_utils
 
@@ -37,15 +27,15 @@ class PoseVisualizer:
     screen_width, screen_height = screen_dims
 
     # Only show the pose on the image if the person is in the frame
-    if pose_landmarks:
+    if image_landmarks:
       # Draw the basic connections between landmarks
-      mp_drawing.draw_landmarks(image_with_pose, pose_landmarks, mp_pose.POSE_CONNECTIONS)
+      mp_drawing.draw_landmarks(image_with_pose, image_landmarks, mp_pose.POSE_CONNECTIONS)
       
       # This loop goes over the joints of the landmarks of the shoulder and elbow for both arms
       for i in range(11, 15):
         # x and y position of the landmark
-        x = round(pose_landmarks.landmark[i].x * screen_width)
-        y = round(pose_landmarks.landmark[i].y * screen_height)
+        x = round(image_landmarks.landmark[i].x * screen_width)
+        y = round(image_landmarks.landmark[i].y * screen_height)
 
         # Draw a circle around the landmark as a visual indicator that this will be sent to the robot
         image_with_pose = cv2.circle(image_with_pose, (x, y), 10, (255, 0, 0), -1)
@@ -72,32 +62,20 @@ class PoseMath:
     return -x, -y, -z
   
   @staticmethod
-  # Get the angle that the joint is rotated at
-  # This will be useful for figuring out the rotations that the servos will be mapped to
   def get_joint_angles(world_landmarks, landmark_index):
-    # Have to access the values differently based on whether the default pose was used or not
-    if isinstance(world_landmarks, dict):
-      joint_point = (
-        world_landmarks[landmark_index]["x"],
-        world_landmarks[landmark_index]["y"],
-        world_landmarks[landmark_index]["z"]
-      )
-      end_point = (
-        world_landmarks[landmark_index + 2]["x"],
-        world_landmarks[landmark_index + 2]["y"],
-        world_landmarks[landmark_index + 2]["z"]
-      )
-    else:
-      joint_point = (
-        world_landmarks[landmark_index].x,
-        world_landmarks[landmark_index].y,
-        world_landmarks[landmark_index].z
-      )
-      end_point = (
-        world_landmarks[landmark_index + 2].x,
-        world_landmarks[landmark_index + 2].y,
-        world_landmarks[landmark_index + 2].z
-      )
+    # This method gets the angles that a joint is rotated at
+    # This will be useful for figuring out the rotations that the servos will be mapped to
+    # There are two angles because the shoulder and elbow can rotate along two different axes
+    joint_point = (
+      world_landmarks.landmark[landmark_index].x,
+      world_landmarks.landmark[landmark_index].y,
+      world_landmarks.landmark[landmark_index].z
+    )
+    end_point = (
+      world_landmarks.landmark[landmark_index + 2].x,
+      world_landmarks.landmark[landmark_index + 2].y,
+      world_landmarks.landmark[landmark_index + 2].z
+    )
     x, y, z = PoseMath.get_relative_pos(joint_point, end_point)
     # This measures how far the joint is rotated laterally (assuming that the person is facing the camera)
     angleXY = round(math.degrees(math.atan2(y, x)))
