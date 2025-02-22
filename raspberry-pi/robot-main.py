3e185653523726436f2b4215d965e1970d8d970f
anay-schottlabs <anay@schottlabs.com>
2025-02-21 16:40:49 -0800

    Convert servo degrees to usable pulse width

--- raspberry-pi/robot-main.py
+++ raspberry-pi/robot-main.py
@@ -1,6 +1,24 @@
 from PCA9685 import PCA9685
 
+# Important constants for servo control
+MAX_SERVO_DEGREES = 180
+MIN_PULSE_MICROSECONDS = 500
+MAX_PULSE_MICROSECONDS = 2500
+
+# The input from the pose tracking is in the form of angles in degrees
+# We have to map these angles rotations of the servo, which are measured by their pulse width (in microseconds)
+def servo_degrees_to_pulse(degrees):
+  factor = degrees / MAX_SERVO_DEGREES
+  pulse_range = MAX_PULSE_MICROSECONDS - MIN_PULSE_MICROSECONDS
+  pulse = factor * pulse_range + MIN_PULSE_MICROSECONDS
+  return pulse
+
 if __name__ == "__main__":
-  # add communication between the computer and the raspberry pi
-  # write robot code to emulate the movements
+  # pwm = PCA9685(0x40, debug=False)
+  # pwm.setPWMFreq(50)
+  # pwm.setServoPulse(channel=0, pulse=servo_degrees_to_pulse(180))
+
+  # Take in data
+  # Decode it into a readable format
+  # Assign all of the servos to positions governed by the decoded values (converted from degrees into pulse)
   pass
