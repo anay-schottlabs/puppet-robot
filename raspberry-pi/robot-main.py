e74a075ad74d902b5748c4b270939806ff320fdb
anay-schottlabs <anay@schottlabs.com>
2025-02-23 15:31:56 -0800

    Designate channels on PCA9685 HAT for arm servos

--- raspberry-pi/robot-main.py
+++ raspberry-pi/robot-main.py
@@ -1,9 +1,27 @@
 from PCA9685 import PCA9685
 
-# Important constants for servo control
+# Needed for conversion from servo degrees to pulse
 MAX_SERVO_DEGREES = 180
 MIN_PULSE_MICROSECONDS = 500
 MAX_PULSE_MICROSECONDS = 2500
+
+# Values for controlling the claw at the end of each arm
+CLAW_OPEN_PULSE = 2500
+CLAW_CLOSE_PULSE = 500
+
+# Servo channels for right arm
+RIGHT_SHOULDER_FWD = 0
+RIGHT_SHOULDER_LAT = 1
+RIGHT_ELBOW_FWD = 2
+RIGHT_SHOULDER_LAT = 3
+RIGHT_CLAW = 4
+
+# Servo channels for left arm
+LEFT_SHOULDER_FWD = 5
+LEFT_SHOULDER_LAT = 6
+LEFT_ELBOW_FWD = 7
+LEFT_ELBOW_LAT = 8
+LEFT_CLAW = 9
 
 # The input from the pose tracking is in the form of angles in degrees
 # We have to map these angles rotations of the servo, which are measured by their pulse width (in microseconds)
