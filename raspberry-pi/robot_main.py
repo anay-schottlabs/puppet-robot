8b639d5b58f45532ccb17313c75a294e9bc5aea7
anay-schottlabs <anay@schottlabs.com>
2025-02-26 21:25:14 -0800

    Change servo rotations upon receiving a message

--- raspberry-pi/robot_main.py
+++ raspberry-pi/robot_main.py
@@ -4,7 +4,7 @@ import sys
 
 # Since the servo_data_format program is in the directory above, we have to append its path
 sys.path.append('../puppet-robot')
-from servo_data_format import TOPIC
+from servo_data_format import ServoPose, TOPIC
 
 client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
 
@@ -16,8 +16,11 @@ def on_connect(client, userdata, flags, reason_code, properties) -> None:
   print("Connected to the broker.")
   client.subscribe(TOPIC)
 
+# When the message is received, emulate the servo angles on the robot
 def on_message(client, userdata, message) -> None:
   print(f"Received Message: {message.payload}")
+  print("Emulating pose on robot...\n")
+  set_servos(servo_pose=ServoPose.from_sendable(message.payload))
 
 def on_subscribe(client, userdata, mid, reason_code_list, properties) -> None:
   if reason_code_list[0].is_failure:
