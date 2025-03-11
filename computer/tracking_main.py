c4a17671f544f0df3112c2840f2a9b517bb683d4
anay-schottlabs <anay@schottlabs.com>
2025-03-10 19:09:10 -0700

    Use threading to send messages asynchronously

--- computer/tracking_main.py
+++ computer/tracking_main.py
@@ -1,18 +1,56 @@
 from pose_tools import PoseTracker, PoseVisualizer
 import cv2
+from threading import Thread, Lock
+from time import sleep
+from mqtt_publisher import MQTTPublisher
+from dotenv import load_dotenv
+import sys, os
+
+# Since the servo_data_format program is in the directory above, we have to append its path
+sys.path.append('../puppet-robot')
+from servo_data_format import ServoPose, TOPIC
+
+# Load the Raspberry Pi's IP address from the .env file
+load_dotenv()
+RASPBERRY_PI_IP_ADDRESS = os.getenv("RASPBERRY_PI_IP_ADDRESS")
+
+# This is a list of the servo values (angles in degrees)
+# They are all defaulted to zero at the start of the program
+servo_pose = ServoPose(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
+
+kill_next_cycle = False
+CYCLE_DELAY_SECONDS = 5
+lock = Lock()
+
+def publish_messages():
+  publisher = MQTTPublisher()
+  publisher.connect(broker_ip=RASPBERRY_PI_IP_ADDRESS)
+
+  while not kill_next_cycle:
+      # Have some delay so that we don't have an uncontrolled amount of messages
+      sleep(CYCLE_DELAY_SECONDS)
+      with lock:
+        # Send the servo pose over to the Raspberry Pi
+        publisher.publish_message(topic=TOPIC, message=servo_pose.to_sendable())
+  
+  publisher.disconnect()
+  print("Closing message publishing thread. The program will now exit.")
 
 if __name__ == "__main__":
   SCREEN_DIMS = (800, 800)
 
   capture = cv2.VideoCapture(0)
-
-  # Make sure that the capture didn't fail to open
-  if not capture.isOpened():
-    print("Failed to open camera feed. Exiting the program.")
-    exit()
+  
+  publish_thread = Thread(target=publish_messages)
+  publish_thread.start()
 
   # Main program loop
   while True:
+    # Make sure that the capture didn't fail to open
+    if not capture.isOpened():
+      print("Failed to open camera feed. Exiting the program.")
+      break
+
     ret, frame = capture.read()
 
     # Make sure that the frame data didn't fail to be read
@@ -28,21 +66,33 @@ if __name__ == "__main__":
 
     processed_frame = PoseVisualizer.show_pose(frame, image_landmarks, world_landmarks, SCREEN_DIMS)
 
-    # Printing some messages on the screen for logging purposes
     if world_landmarks:
-      # A box so that we can see the text
-      processed_frame = cv2.rectangle(processed_frame, (0, 0), (SCREEN_DIMS[0], 100), (255, 255, 255), -1)
+      # Have to make sure that the lock is not acquired
+      # This is needed to ensure that there is no conflict between the main thread writing to the servo pose while the publish thread is reading from it
+      if not lock.locked():
+        # MAIN CALCULATIONS
+        servo_pose.torso = PoseTracker.get_torso_rotation(world_landmarks=world_landmarks)
 
-      # Checking if hands are closed or open
-      # This may seem weird (LEFT is mapped to Right, while RIGHT is mapped to Left) but they are inverted (for some reason)
-      left_hand = "closed" if PoseTracker.is_hand_grabbing(frame, 'Right') else "open"
-      right_hand = "closed" if PoseTracker.is_hand_grabbing(frame, 'Left') else "open"
-      text = f"LEFT HAND: {left_hand}, RIGHT HAND: {right_hand}"
-      processed_frame = cv2.putText(processed_frame, text, (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        # This may seem weird (left is mapped to right, while right is mapped to left), but the handedness values seem to be inverted as of my testing
+        is_left_hand_closed = PoseTracker.is_hand_grabbing(frame, "Right")
+        servo_pose.left_claw = ServoPose.CLAW_CLOSE_DEGREES if is_left_hand_closed else ServoPose.CLAW_OPEN_DEGREES
+        is_right_hand_closed = PoseTracker.is_hand_grabbing(frame, "Left")
+        servo_pose.right_claw = ServoPose.CLAW_CLOSE_DEGREES if is_right_hand_closed else ServoPose.CLAW_OPEN_DEGREES
 
-      # Finding the torso rotation
-      text = f"TORSO: {PoseTracker.get_torso_rotation(world_landmarks=world_landmarks)}"
-      processed_frame = cv2.putText(processed_frame, text, (50, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+        # LOGGING
+        # A box so that we can see the text
+        processed_frame = cv2.rectangle(processed_frame, (0, 0), (SCREEN_DIMS[0], 100), (255, 255, 255), -1)
+
+        # Checking if hands are closed or open
+        # This may seem weird (LEFT is mapped to Right, while RIGHT is mapped to Left) but they are inverted (for some reason)
+        left_hand = "closed" if is_left_hand_closed else "open"
+        right_hand = "closed" if is_right_hand_closed else "open"
+        text = f"LEFT HAND: {left_hand}, RIGHT HAND: {right_hand}"
+        processed_frame = cv2.putText(processed_frame, text, (50, 50), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
+
+        # Finding the torso rotation
+        text = f"TORSO: {servo_pose.torso}"
+        processed_frame = cv2.putText(processed_frame, text, (50, 100), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 2)
 
     # Display the current frame image after it has been processed
     cv2.imshow("Pose Tracking", processed_frame)
@@ -55,3 +105,4 @@ if __name__ == "__main__":
   # Release the capture and close the window once the program is finished
   capture.release()
   cv2.destroyAllWindows()
+  kill_next_cycle = True
